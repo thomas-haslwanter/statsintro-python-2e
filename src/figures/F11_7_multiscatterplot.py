91d3036e17ba64480cfcacad23eb129ff23bb283
Thomas Haslwanter <thomas.haslwanter@fh-linz.at>
2021-09-19 13:39:46 +0200

    renamings during cleanup and tests

---
+++ src/figures/F11_7_multiscatterplot.py
@@ -0,0 +1,44 @@
+"""Multiple Regression
+- Shows how to calculate the best fit to a plane in 3D, and how to find the
+  corresponding statistical parameters.
+- Demonstrates how to make a 3d plot.
+- Example of multiscatterplot, for visualizing correlations in three- to
+  six-dimensional datasets.
+"""
+
+# author: Thomas Haslwanter, date: Sept-2021
+
+# Import standard packages
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# additional packages
+import sys
+import os
+from typing import Tuple, List
+
+sys.path.append(os.path.join('..', '..', 'Utilities'))
+
+try:
+# Import formatting commands if directory "Utilities" is available
+    from ISP_mystyle import showData 
+    
+except ImportError:
+# Ensure correct performance otherwise
+    def showData(*options):
+        plt.show()
+        return
+
+
+def scatterplot() -> None:
+    """Fancy scatterplots, using the package "seaborn" """
+    
+    df = sns.load_dataset("iris")
+    sns.pairplot(df, hue="species", size=2.5)    
+    showData('multiScatterplot.jpg')
+    
+    
+if __name__ == '__main__':
+    scatterplot()
---
+++ src/listings/L11_1_corr_matrix.py
@@ -0,0 +1,42 @@
+""" Plotting a diagonal correlation matrix
+
+With permission from Michael Waskom, from
+http://seaborn.pydata.org/examples/many_pairwise_correlations.html
+"""
+
+from string import ascii_letters
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_theme(style="white")
+
+# Generate a large random dataset
+# The syntax here is sligthly different from the previously
+# used np.random.seed. For details, see
+# https://stackoverflow.com/questions/5836335/consistently-create-same-random-numpy-array
+rs = np.random.RandomState(33)
+df = pd.DataFrame(data=rs.normal(size=(100, 26)),
+                 columns=list(ascii_letters[26:]))
+
+# Compute the correlation matrix
+corr = df.corr()
+
+# Generate a mask for the upper triangle
+mask = np.triu(np.ones_like(corr, dtype=bool))
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+           square=True, linewidths=.5, cbar_kws={"shrink": .5})
+out_file = 'many_pairwise_correlations.jpg'
+plt.savefig(out_file, dpi=300)
+print(f'Correlation-matrix saved to {out_file}')
+
+plt.show()
